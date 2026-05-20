2c3c127377b9bb93878c3be91b9d580a7439c1cf
Gustavo Donizete <98923046+GustavoDonizete@users.noreply.github.com>
2026-05-20 17:04:07 -0300

    Delete integrador_eventos1.2 directory

--- integrador_eventos1.2/static/script.js
+++
@@ -1,8 +0,0 @@
-// Auto-dismiss alertas de sucesso após 5s
-document.addEventListener('DOMContentLoaded', () => {
-  setTimeout(() => {
-    document.querySelectorAll('.alert-success').forEach(el =>
-      bootstrap.Alert.getOrCreateInstance(el)?.close()
-    );
-  }, 5000);
-});
